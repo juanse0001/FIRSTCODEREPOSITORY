72e6033898b855b73e179c9c603e7fca2563a00d
Juan Sebastian Odoñez Sierra <juanseordo2206@outlook.com>
2024-12-31 16:42:50 -0500

    feat(Back-end): Changes connection string

--- BibliotecaWebApplication/BibliotecaWebApplication/Models/AutorLibro.cs
+++ BibliotecaWebApplication/BibliotecaWebApplication/Models/AutorLibro.cs
@@ -2,7 +2,7 @@
 {
     public class AutorLibro
     {
-        public Guid AutorId { get; set; }
+        public Guid AutorId { get; set; } 
         public Autor Autor { get; set; }
         public Guid LibroId { get; set; }
         public Libro Libro { get; set; }
--- BibliotecaWebApplication/BibliotecaWebApplication/obj/Debug/net7.0/BibliotecaWebApplication.AssemblyInfo.cs
+++ BibliotecaWebApplication/BibliotecaWebApplication/obj/Debug/net7.0/BibliotecaWebApplication.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b437087eee6c95c7a70c6950d3650036a6760071")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02bd72797fe189d13fe325be956638c4b0bd7ee2")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
