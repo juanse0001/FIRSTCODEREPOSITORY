376673cff5431f499761f32d9a9404c7835beff5
Juan Sebastian Odoñez Sierra <juanseordo2206@outlook.com>
2025-01-28 19:34:16 -0500

    feat(Front-end): Adding styles in the file css and index AutorLibros

--- BibliotecaWebApplication/BibliotecaWebApplication/obj/Debug/net7.0/BibliotecaWebApplication.AssemblyInfo.cs
+++ BibliotecaWebApplication/BibliotecaWebApplication/obj/Debug/net7.0/BibliotecaWebApplication.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b355a5ac95d33c5ad373404e72e238eddccdc674")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edde11c183204d59c2ca3d486ad08bfdb8a57cf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BibliotecaWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BibliotecaWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
